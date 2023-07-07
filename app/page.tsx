cc76bba6f9dab5bf358fd605fafd141ee923e1c7
dikiogres <dikiogres@gmail.com>
2023-07-07 14:53:52 +0700

    feat: fetch post structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,60 @@
-const Home = () => {
+import { fetchAllProjects } from "@/lib/action";
+import { ProjectInterface } from "@/types/common.types";
+
+type SearchParams = {
+  category?: string | null;
+  endcursor?: string | null;
+}
+
+type Props = {
+  searchParams: SearchParams
+}
+
+type ProjectSearch = {
+  projectSearch: {
+    edges: { node: ProjectInterface }[];
+    pageInfo: {
+      hasPreviousPage: boolean;
+      hasNextPage: boolean;
+      startCursor: string;
+      endCursor: string;
+    };
+  },
+}
+
+
+const Home = async ({ searchParams: { category, endcursor } }: Props) => {
+  const data = await fetchAllProjects(category, endcursor) as ProjectSearch
+
+  const projectsToDisplay = data?.projectSearch?.edges || [];
+
+  if (projectsToDisplay.length === 0) {
+    return (
+      <section className="flexStart flex-col paddings">
+
+
+        Categories
+
+        <p className="no-result-text text-center">No projects found, go create some first.</p>
+      </section>
+    )
+  }
   return (
     <section className="flexStart flex-col paddings mb-16">
-      <h1>Categories</h1>
-      <h1>Posts</h1>
-      <h1>LoadMore</h1>
+
+      Categories
+
+      <section className="projects-grid">
+        {projectsToDisplay.map(({ node }: { node: ProjectInterface }) => (
+
+          <>
+            <h1>Project Card</h1>
+          </>
+        ))}
+      </section>
+
+      Load More
+
     </section>
   );
 };
